22bda14b195133a648cbb4419377d7f84333d7a2
Fergus Cooper <3770306+fcooper8472@users.noreply.github.com>
2025-07-08 14:46:00 +0000

    Add flora the flamingo

---
+++ src/zoo/flamingo.py
@@ -0,0 +1,15 @@
+
+# elephant.py
+
+from .animal import Animal
+
+
+class Flamingo(Animal):
+    def __init__(self, name="Flora"):
+        super().__init__(name, species="Flamingo")
+
+    def sound(self):
+        return "honks"
+
+    def action(self):
+        return "balances gracefully on one leg!"
